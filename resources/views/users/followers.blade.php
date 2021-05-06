389d8f98266af065968aebcaae6c14c7fb9d6c01
dokoro <dokoro.17@ymobile.ne.jp>
2021-05-06 18:05:45 +0000

    follow, unfollow

--- resources/views/users/followers.blade.php
+++ resources/views/users/followers.blade.php
@@ -5,12 +5,9 @@
         <aside class="col-sm-4">
             @include('users.card')
         </aside>
-        <div class"col-sm-8">
+        <div class="col-sm-8">
             @include('users.navtabs')
-            @if (Auth::id() == $user->id)
-                @include('microposts.form')
-            @endif
-            @include('microposts.microposts')
+            @include('users.users')
         </div>
     </div>
 @endsection
